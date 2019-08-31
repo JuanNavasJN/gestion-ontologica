39048468159f83f523b26649b3d1852ec43b4c45
Juan Navas <navascruzf@gmail.com>
2019-08-31 10:52:53 -0400

    remove material dashboard

--- dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('dash', views.HomePageView.as_view(), name='index'),
-    path('', views.LoginPageView.as_view(), name='login')
-    # path('dashboard', views.DashboardView.as_view(), name='dashboard'),
-]
